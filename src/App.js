d5a90b0d34b8ebe61ea08b0c546335bced69310c
unknown <n7haat00@student.oamk.fi git config --global user.name>
2021-10-11 05:29:05 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
+import { Meme } from './Meme'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const objectToQueryParam = obj => {
+  const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`)
+  return '?' + params.join('&')
 }
 
-export default App;
+function App () {
+  const [templates, setTemplates] = useState([])
+  const [template, setTemplate] = useState(null)
+  const [topText, setTopText] = useState('')
+  const [bottomText, setBottomText] = useState('')
+  const [meme, setMeme] = useState(null)
+  
+
+  
+    function refreshPage() {
+      window.location.reload(false);
+    }
+  
+    useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes').then(x =>
+      x.json().then(response => setTemplates(response.data.memes))
+    )
+  }, [])
+
+  if (meme) {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <img style={{ width: 400 }} src={meme} alt='custom meme' />
+        <button onClick={refreshPage}>Uudestaan</button>
+      </div>
+    )
+  }
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      {template && (
+        <form
+          onSubmit={async e => {
+            e.preventDefault()
+            const params = {
+              template_id: template.id,
+              text0: topText,
+              text1: bottomText,
+              username: 'MemeKayttaja',
+              password: 'MemeSalasana'
+              
+            }
+            const response = await fetch(
+              `https://api.imgflip.com/caption_image${objectToQueryParam(
+                params
+              )}`
+            )
+            const json = await response.json()
+            setMeme(json.data.url)
+          }}
+        >
+          <Meme template={template} />
+          <input
+            placeholder='1. teksti'
+            value={topText}
+            onChange={e => setTopText(e.target.value)}
+          />
+          <input
+            placeholder='2. teksti'
+            value={bottomText}
+            onChange={e => setBottomText(e.target.value)}
+          />
+          <button type='submit'>Luo meemi</button>
+          <button onClick={refreshPage}>Takaisin</button>
+        </form>
+      )}
+      {!template && (
+        <>
+          <h1>Valitse meemi</h1>
+          {templates.map(template => {
+            return (
+              <Meme
+                template={template}
+                onClick={() => {
+                  setTemplate(template)
+                }}
+              />
+            )
+          })}
+        </>
+      )}
+    </div>
+  )
+}
+
+export default App
